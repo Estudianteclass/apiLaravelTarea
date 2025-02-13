e212e927632d26c4e60e72e8be202d65c6dd1fe1
gabriel <gabrielpr9520@gmail.com>
2025-02-13 17:45:54 +0000

    ejercicio api

--- app/Http/Controllers/ApiProductModel.php
+++ app/Http/Controllers/ApiProductModel.php
@@ -4,40 +4,58 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
-
+/**
+* @OA\Info(title="API productos", version="1.0")
+*
+* @OA\Server(url="http://localhost:8000")
+*/
 class ApiProductModel extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
+
+/**
+    * @OA\Get(
+    *     path="/api/products",
+    *     summary="Mostrar productos",
+    *     @OA\Response(
+    *         response=200,
+    *         description="Mostrar todos los productos."
+    *     ),
+    *     @OA\Response(
+    *         response="default",
+    *         description="Ha ocurrido un error."
+    *     )
+    * )
+    */
+
     public function index()
     {
-     $products=Product::all();
-     return response()->json([
-        'status'=>true,
-        'prodducts'=>$products
-     ]);
+        $products = Product::all();
+        return response()->json([
+            'status' => true,
+            'prodducts' => $products
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      * https://kinsta.com/es/blog/laravel-api/
      */
-    public function create()
-    {
-        //
-    }
+    public function create() {}
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-       $product= Product::create($request->all());
+        $product = Product::create($request->all());
         return response()->json([
-            'status'=>true,
-            'message'=>'producto añadido correctamente',
-            'product'=>$product,
+            'status' => true,
+            'message' => 'producto añadido correctamente',
+            'product' => $product,
         ]);
     }
 
@@ -46,23 +64,35 @@ class ApiProductModel extends Controller
      */
     public function show(Product $product)
     {
-        //
+        $found = Product::find($product->id);
+        if ($found) {
+            return response()->json([
+                'status' => true,
+                'message' => 'producto encontrado',
+                'product' => $found
+            ]);
+        } else {
+
+            return response()->json([
+                'status' => true,
+                'message' => 'producto no encontrado',
+                'product' => null
+            ]);
+        }
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
-    {
-        //
-    }
+    public function edit(Product $product) {}
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
+        return response()->json($product);
     }
 
     /**
@@ -70,6 +100,6 @@ class ApiProductModel extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,16 @@
 use App\Http\Controllers\ApiProductModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::apiResource('products',ApiProductModel::class);
+// Route::apiResource('products',ApiProductModel::class);
+
+//Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('products', ApiProductModel::class);
+//});
+
+Route::post("login",[UserController::class,'index']);
